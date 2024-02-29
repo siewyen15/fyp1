4b75625f15ac3b35b1813d83899ef6e7f9a912e4
weihan <limwh-wp21@student.tarc.edu.my>
2024-03-01 03:20:25 +0800

    Initial Commit

--- app/src/main/java/com/example/fyp1/ProfileActivity.kt
+++ app/src/main/java/com/example/fyp1/ProfileActivity.kt
@@ -1,11 +1,19 @@
 package com.example.fyp1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
     }
+
+
 }
